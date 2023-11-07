45b0884bcc69f47b4a8bc3bceb9d1b75ede3badb
Stephen Jefferson <stephen@sjefferson.co.uk>
2023-11-07 22:50:34 +0000

    Fixed processing overhead logic

--- multicore.py
+++ multicore.py
@@ -7,31 +7,36 @@ from math import pi
 class Multicore_Weather_Wind:
 	def __init__(self) -> None:
 		self.pending_wind_data_lock = _thread.allocate_lock()
-		self.quarterseconds_lock = _thread.allocate_lock()
+		self.samples_lock = _thread.allocate_lock()
 		self.wind_speed_pin = Pin(9, Pin.IN, Pin.PULL_UP)
-		self.quarterseconds = [[]] * (60 * 4)
-		self.quarterseconds_max_list_id = len(self.quarterseconds) - 1
+		self.monitoring_window_s = 60
+		self.sample_hz = 4
+		self.sample_ms = 1000 / self.sample_hz
+		self.samples = [[]] * (self.monitoring_window_s * self.sample_hz)
+		self.samples_max_list_id = len(self.samples) - 1
 		self.WIND_CM_RADIUS = 7.0
 		self.WIND_FACTOR = 0.0218
 		self.pending_wind_data = []
 		self.debug = True
 		self.processing_overhead_poll_count = 0
-		self.last_loop_overhead = 0
+		self.last_loop_overhead_ms = 0
+		self.remaining_loop_overhead_ms = 0
     
 	def init_wind_poll_thread(self) -> None:
 		self.wind_poll_thread = _thread.start_new_thread(self.constant_poll_wind_speed, ())
 
 	def discard_overhead_compensation_poll(self) -> None:		
-		if self.last_loop_overhead >= 250:
-			self.last_loop_overhead -= 250
+		if self.remaining_loop_overhead_ms >= self.sample_ms:
+			self.remaining_loop_overhead_ms -= self.sample_ms
 		else:
 			start = ticks_ms()
-			while ticks_diff(ticks_ms(), start) <= 250 - self.last_loop_overhead:
+			while ticks_diff(ticks_ms(), start) <= (self.sample_ms - self.last_loop_overhead_ms):
 				pass
+			self.remaining_loop_overhead_ms = 0
 		
 		self.processing_overhead_poll_count += 1
 	
-	def quartersecond_wind_poll(self) -> list:
+	def sample_wind_poll(self) -> list:
 		previous_wind_pin_state = self.wind_speed_pin.value()
 		ticks = []
 		start = ticks_ms()
@@ -43,41 +48,42 @@ class Multicore_Weather_Wind:
 		
 		return ticks
 	
-	def record_quartersecond_datapoint(self, quartersecond_id) -> None:
-		ticks = self.quartersecond_wind_poll()
-		with self.quarterseconds_lock:
-			self.quarterseconds[quartersecond_id] = ticks
+	def record_sample_datapoint(self, sample_id) -> None:
+		ticks = self.sample_wind_poll()
+		with self.samples_lock:
+			self.samples[sample_id] = ticks
 
 		if self.debug:
-			print("Quartersecond {} has value: {}".format(quartersecond_id, self.quarterseconds[quartersecond_id]))
+			print("sample {} has value: {}".format(sample_id, self.samples[sample_id]))
 		
 	def append_pending_wind_data(self, wind_data) -> None:
 		with self.pending_wind_data_lock:
 			self.pending_wind_data.append(wind_data)
 	
 	def calculate_processing_overhead(self) -> None:
-		time_now = ticks_ms()
-		processing_time = time_now - self.previous_loop_time
-		self.previous_loop_time = time_now
-		self.last_loop_overhead = processing_time - (60 * 1000)
+		time_now_ms = ticks_ms()
+		processing_time_ms = time_now_ms - self.previous_loop_time_ms
+		self.previous_loop_time_ms = time_now_ms
+		self.last_loop_overhead_ms = processing_time_ms - ((self.monitoring_window_s * 1000) - self.last_loop_overhead_ms)
+		self.remaining_loop_overhead_ms = self.last_loop_overhead_ms
+		self.processing_overhead_poll_count = 0
 		if self.debug:
-			print("Processing overhead: {}".format(self.last_loop_overhead))
+			print("Processing overhead: {}".format(self.last_loop_overhead_ms))
 	
 	def constant_poll_wind_speed(self) -> None:
-		self.previous_loop_time = ticks_ms()
-		quartersecond_id = 0
+		self.previous_loop_time_ms = ticks_ms()
+		sample_id = 0
 		
 		while (True):
-			if self.last_loop_overhead > 0:
+			if self.remaining_loop_overhead_ms > 0:
 				self.discard_overhead_compensation_poll()
-				quartersecond_id += 1
 			else:
-				self.record_quartersecond_datapoint(quartersecond_id)
+				self.record_sample_datapoint(sample_id)
 			
-			if quartersecond_id < self.quarterseconds_max_list_id:
-				quartersecond_id += 1
+			if sample_id < self.samples_max_list_id:
+				sample_id += 1
 			else:
-				quartersecond_id = 0
+				sample_id = 0
 				self.append_pending_wind_data(self.process_wind_data())
 				self.calculate_processing_overhead()
 				
@@ -94,7 +100,7 @@ class Multicore_Weather_Wind:
 
 	def convert_qs_list_ticks_to_average_ms(self) -> list:
 		qs_average_tick_ms = []
-		for qs in self.cached_quarterseconds:
+		for qs in self.cached_samples:
 			if len(qs) > 1:
 				average_tick_ms = (ticks_diff(qs[-1], qs[0])) / (len(qs) - 1)
 				qs_average_tick_ms.append(average_tick_ms)
@@ -121,15 +127,15 @@ class Multicore_Weather_Wind:
 
 		return gust_wind_speed
 
-	def cache_quarterseconds(self) -> None:
-		with self.quarterseconds_lock:
-			self.cached_quarterseconds = self.quarterseconds
+	def cache_samples(self) -> None:
+		with self.samples_lock:
+			self.cached_samples = self.samples
 	
 	def remove_processing_overhead_data_polls(self) -> None:
-		self.cached_quarterseconds = self.cached_quarterseconds[0 + self.processing_overhead_poll_count : -1]
+		self.cached_samples = self.cached_samples[0 + self.processing_overhead_poll_count : -1]
 	
 	def process_wind_data(self) -> dict[str, float]:
-		self.cache_quarterseconds()
+		self.cache_samples()
 		self.remove_processing_overhead_data_polls()
 
 		average_wind = self.calculate_average_wind()
